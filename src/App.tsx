b478018e2ae511653e09c6febb25ff9c8cf0fcd4
Huy Dang <hdang.log@gmail.com>
2020-04-23 22:32:36 -0700

    Create a simple LoginForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import Dialog from "@reach/dialog"
 
-function App() {
+import Logo from "./components/logo"
+import "@reach/dialog/styles.css"
+
+const App = () => {
+  const [openModal, setOpenModal] = useState<"none" | "login" | "register">(
+    "none"
+  )
+
+  const login = (formData: { username: string; password: string }) => {
+    console.log("login", formData)
+  }
+
+  const register = (formData: { username: string; password: string }) => {
+    console.log("register", formData)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Logo width={80} height={80} />
+      <h1>BookShelf</h1>
+      <div>
+        <button onClick={() => setOpenModal("login")}>Login</button>
+      </div>
+      <div>
+        <button onClick={() => setOpenModal("register")}>Register</button>
+      </div>
+
+      <Dialog aria-label="Login Form" isOpen={openModal === "login"}>
+        <div>
+          <button onClick={() => setOpenModal("none")}>Close</button>
+        </div>
+        <LoginForm buttonText="Login" onSubmit={login} />
+      </Dialog>
+
+      <Dialog aria-label="Register Form" isOpen={openModal === "register"}>
+        <div>
+          <button onClick={() => setOpenModal("none")}>Close</button>
+        </div>
+        <LoginForm buttonText="Register" onSubmit={register} />
+      </Dialog>
     </div>
-  );
+  )
 }
 
-export default App;
+const LoginForm: React.FC<{
+  onSubmit: ({
+    username,
+    password,
+  }: {
+    username: string
+    password: string
+  }) => void
+  buttonText: string
+}> = ({ onSubmit, buttonText }) => {
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+
+    const elements = (event.target as HTMLFormElement).elements
+    const username = elements.namedItem("username") as HTMLInputElement
+    const password = elements.namedItem("password") as HTMLInputElement
+
+    onSubmit({
+      username: username.value,
+      password: password.value,
+    })
+  }
+  return (
+    <>
+      <h3>{buttonText}</h3>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <div>
+          <button type="submit">{buttonText}</button>
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default App
