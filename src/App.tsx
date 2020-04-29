aae8dbb42dafc6112a0bf5a724d9143ff781fa56
Huy Dang <hdang.log@gmail.com>
2020-04-28 22:35:44 -0700

    Style Login and Register form

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import React, { useState } from "react"
-import Dialog from "@reach/dialog"
+/** @jsx jsx */
+/** @jsxFrag React.Fragment */
+import { jsx } from "@emotion/core"
+import "bootstrap/dist/css/bootstrap-reboot.css"
 
-import Logo from "./components/logo"
+import React from "react"
+import VisuallyHidden from "@reach/visually-hidden"
+
+import {
+  Modal,
+  ModalDismissButton,
+  ModalOpenButton,
+  ModalContents,
+} from "components/modal"
+import Logo from "components/logo"
+import { Button, CircleButton, FormGroup, Input, Spinner } from "components/lib"
+
 import "@reach/dialog/styles.css"
 
 const App = () => {
-  const [openModal, setOpenModal] = useState<"none" | "login" | "register">(
-    "none"
-  )
-
   const login = (formData: { username: string; password: string }) => {
     console.log("login", formData)
   }
@@ -18,34 +27,57 @@ const App = () => {
   }
 
   return (
-    <div>
+    <div
+      css={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
       <Logo width={80} height={80} />
-      <h1>BookShelf</h1>
-      <div>
-        <button onClick={() => setOpenModal("login")}>Login</button>
+      <h1>Bookshelf</h1>
+      <div
+        css={{
+          display: "grid",
+          gridTemplateColumns: "repeat(2, minmax(0, 1fr))",
+          gridGap: "0.75rem",
+        }}
+      >
+        <Modal>
+          <ModalOpenButton>
+            <Button variant="primary">Login</Button>
+          </ModalOpenButton>
+          <ModalContents aria-label="Login form">
+            {circleDismissButton}
+            <h3 css={{ textAlign: "center", fontSize: "2em" }}>Login</h3>
+            <LoginForm
+              onSubmit={login}
+              submitButton={<Button variant="primary">Login</Button>}
+            />
+          </ModalContents>
+        </Modal>
+        <Modal>
+          <ModalOpenButton>
+            <Button variant="secondary">Register</Button>
+          </ModalOpenButton>
+          <ModalContents aria-label="Registration form">
+            {circleDismissButton}
+            <h3 css={{ textAlign: "center", fontSize: "2em" }}>Register</h3>
+            <LoginForm
+              onSubmit={register}
+              submitButton={<Button variant="secondary">Register</Button>}
+            />
+          </ModalContents>
+        </Modal>
       </div>
-      <div>
-        <button onClick={() => setOpenModal("register")}>Register</button>
-      </div>
-
-      <Dialog aria-label="Login Form" isOpen={openModal === "login"}>
-        <div>
-          <button onClick={() => setOpenModal("none")}>Close</button>
-        </div>
-        <LoginForm buttonText="Login" onSubmit={login} />
-      </Dialog>
-
-      <Dialog aria-label="Register Form" isOpen={openModal === "register"}>
-        <div>
-          <button onClick={() => setOpenModal("none")}>Close</button>
-        </div>
-        <LoginForm buttonText="Register" onSubmit={register} />
-      </Dialog>
     </div>
   )
 }
 
-const LoginForm: React.FC<{
+type LoginFormProps = {
   onSubmit: ({
     username,
     password,
@@ -53,8 +85,10 @@ const LoginForm: React.FC<{
     username: string
     password: string
   }) => void
-  buttonText: string
-}> = ({ onSubmit, buttonText }) => {
+  submitButton: React.ReactElement
+}
+
+const LoginForm: React.FC<LoginFormProps> = ({ onSubmit, submitButton }) => {
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault()
 
@@ -68,23 +102,45 @@ const LoginForm: React.FC<{
     })
   }
   return (
-    <>
-      <h3>{buttonText}</h3>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
-        </div>
-        <div>
-          <button type="submit">{buttonText}</button>
-        </div>
-      </form>
-    </>
+    <form
+      onSubmit={handleSubmit}
+      css={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "stretch",
+        "> div": {
+          margin: "10px auto",
+          width: "100%",
+          maxWidth: "300px",
+        },
+      }}
+    >
+      <FormGroup>
+        <label htmlFor="username">Username</label>
+        <Input type="text" id="username" autoComplete="off" />
+      </FormGroup>
+      <FormGroup>
+        <label htmlFor="password">Password</label>
+        <Input type="password" id="password" autoComplete="off" />
+      </FormGroup>
+      <div>
+        {React.cloneElement(submitButton, { type: "submit" })}
+        <Spinner css={{ marginLeft: 5 }} />
+      </div>
+    </form>
   )
 }
+
+const circleDismissButton = (
+  <div css={{ display: "flex", justifyContent: "flex-end" }}>
+    {" "}
+    <ModalDismissButton>
+      <CircleButton>
+        <VisuallyHidden>Close</VisuallyHidden>
+        <span aria-hidden>×</span>
+      </CircleButton>
+    </ModalDismissButton>
+  </div>
+)
 
 export default App
