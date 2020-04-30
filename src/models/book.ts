80fe9399014b8ca466196bafd587be23269aa85e
Huy Dang <hdang.log@gmail.com>
2020-04-29 21:20:04 -0700

    Add typings

---
+++ src/models/book.ts
@@ -0,0 +1,9 @@
+export type Book = {
+  id: string
+  title: string
+  author: string
+  coverImageUrl: string
+  pageCount: number
+  publisher: string
+  synopsis: string
+}
---
+++ src/models/list-item.ts
@@ -0,0 +1,9 @@
+export type ListItem = {
+  id: string
+  bookId: string
+  ownerId: string
+  rating: number
+  notes: string
+  startDate: number
+  finishDate: number
+}
