d36f0a50e73212cf03e37fe0793a927ac097f100
Huy Dang <hdang.log@gmail.com>
2020-05-16 16:23:54 -0700

    Implement auth-client

---
+++ src/utils/auth-client.ts
@@ -0,0 +1,25 @@
+import {client, localStorageKey} from './api-client'
+import {User, UserForm} from 'models/user'
+
+function handleUserResponse({user: {token, ...user}}: {user: User}) {
+  window.localStorage.setItem(localStorageKey, token as string)
+  return user
+}
+
+function login({username, password}: UserForm) {
+  return client<{user: User}>('/login', {body: {username, password}}).then(
+    handleUserResponse,
+  )
+}
+
+function register({username, password}: UserForm) {
+  return client<{user: User}>('/register', {body: {username, password}}).then(
+    handleUserResponse,
+  )
+}
+
+function logout() {
+  window.localStorage.removeItem(localStorageKey)
+}
+
+export {login, register, logout}
