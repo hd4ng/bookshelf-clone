5c611a3e44223057dfcc752cf5cfb2a22d3f19ce
Huy Dang <hdang.log@gmail.com>
2020-05-17 11:07:32 -0700

    Implement book screen

---
+++ src/screens/book.tsx
@@ -0,0 +1,62 @@
+/** @jsx jsx */
+/** @jsxFrag React.Fragment */
+import {jsx} from '@emotion/core'
+
+import React from 'react'
+import {useParams} from 'react-router-dom'
+import * as booksClient from 'utils/books-client'
+import * as mq from 'styles/media-queries'
+import {useAsync} from 'utils/use-async'
+import {loadingBook} from 'utils/book-placeholder'
+import {Book} from 'models/book'
+
+function BookScreen() {
+  const {bookId} = useParams()
+  const {data, run} = useAsync<{book: Book}>()
+
+  React.useEffect(() => {
+    run(booksClient.read(bookId))
+  }, [bookId, run])
+
+  const {title, author, coverImageUrl, publisher, synopsis} =
+    data?.book ?? loadingBook
+
+  return (
+    <div>
+      <div
+        css={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 2fr',
+          gridGap: '2em',
+          marginBottom: '1em',
+          [mq.small]: {
+            display: 'flex',
+            flexDirection: 'column',
+          },
+        }}
+      >
+        <img
+          src={coverImageUrl}
+          alt={`${title} book cover`}
+          css={{width: '100%', maxWidth: '14rem'}}
+        />
+        <div>
+          <div css={{display: 'flex', position: 'relative'}}>
+            <div css={{flex: 1, justifyContent: 'space-between'}}>
+              <h1>{title}</h1>
+              <div>
+                <i>{author}</i>
+                <span css={{marginLeft: 6, marginRight: 6}}>|</span>
+                <i>{publisher}</i>
+              </div>
+            </div>
+          </div>
+          <br />
+          <p>{synopsis}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export {BookScreen}
