7fe27ef53bd906f27ed4fd63a32d0f60882b7d70
Huy Dang <hdang.log@gmail.com>
2020-05-20 19:34:01 -0700

    Create hook for handling api call via react-query

---
+++ src/utils/list-items.ts
@@ -0,0 +1,41 @@
+import {useQuery, useMutation} from 'react-query'
+import * as listItemsClient from './list-items-client'
+import {Item} from 'models/list-item'
+
+function readListItems() {
+  return listItemsClient.read().then(data => data.listItems)
+}
+
+function useListItems() {
+  const {data} = useQuery('list-items', readListItems)
+  return data ?? []
+}
+
+function useListItem(bookId: string) {
+  const listItems = useListItems()
+  return listItems.find(li => li.bookId === bookId) ?? null
+}
+
+function useUpdateListItem() {
+  return useMutation((updates: Item) =>
+    listItemsClient.update(updates.id, updates),
+  )
+}
+
+function useRemoveListItem() {
+  return useMutation(({id}: {id: string}) => listItemsClient.remove(id))
+}
+
+function useCreateListItem() {
+  return useMutation(({bookId}: {bookId: string}) =>
+    listItemsClient.create(bookId),
+  )
+}
+
+export {
+  useCreateListItem,
+  useListItems,
+  useListItem,
+  useUpdateListItem,
+  useRemoveListItem,
+}
