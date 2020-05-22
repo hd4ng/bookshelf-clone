e376518625358fd1694b93633e7bfb01b1d529c4
Huy Dang <hdang.log@gmail.com>
2020-05-22 15:44:33 -0700

    Wrap the <App /> in a <ReactQueryConfigProvider />

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,20 @@ import {loadDevTools} from 'dev-tools/load'
 import './bootstrap'
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import {ReactQueryConfigProvider, ReactQueryProviderConfig} from 'react-query'
 import {App} from './app'
+
+const queryConfig: ReactQueryProviderConfig = {
+  useErrorBoundary: true,
+  refetchAllOnWindowFocus: false,
+}
 
 loadDevTools(() => {
   ReactDOM.render(
     <React.StrictMode>
-      <App />
+      <ReactQueryConfigProvider config={queryConfig}>
+        <App />
+      </ReactQueryConfigProvider>
     </React.StrictMode>,
     document.getElementById('root'),
   )
